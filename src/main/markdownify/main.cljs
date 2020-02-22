5414b8ad8f4068494c7ed98b43457abd9471d5bb
Vincent Raerek <vincent@monadnomad.dev>
2020-02-22 13:34:48 -0800

    Add flash messages for UX feedback

--- src/main/markdownify/main.cljs
+++ src/main/markdownify/main.cljs
@@ -2,6 +2,18 @@
   (:require [reagent.core :as reagent]
             ["showdown" :as showdown]))
 
+(defonce flash-message (reagent/atom nil))
+(defonce flash-timeout (reagent/atom nil))
+
+(defn flash
+  ([text]
+   (flash text 3000))
+  ([text ms]
+   (js/clearTimeout @flash-timeout)
+   (reset! flash-message text)
+   (reset! flash-timeout
+           (js/setTimeout #(reset! flash-message nil) ms))))
+
 (defonce showdown-converter (showdown/Converter.))
 
 (defn md->html [md]
@@ -41,7 +53,24 @@
       (-> js/document .getSelection (.addRange selected)))))
 
 (defn app []
-  [:div
+  [:div {:style {:position "relative"}}
+   [:div
+    {:style {:position "absolute"
+             :margin :auto
+             :left 0
+             :right 0
+             :text-align :center
+             :max-width 200
+             :padding "2em"
+             :background-color :yellow
+             :z-index 1
+             :border-radius 10
+             :transform (if @flash-message
+                          "scaleY(1)"
+                          "scaleY(0)")
+             :transition "transform 0.2s ease-out" 
+             }}
+    @flash-message]
    [:h1 "Markdownify"]
 
    [:div {:style {:display :flex}}
@@ -56,14 +85,17 @@
                :height "500px"
                :width "100%"}}]
      [:button
-      {:on-click #(copy-to-clipboard (->md @text-state))
+      {:on-click (fn []
+                   (copy-to-clipboard (->md @text-state))
+                   (flash "Markdown copied to clipboard!"))
        :style {:background-color :green
                :padding "1em"
                :color :white
                :border-radius 10}}
       "Copy Markdown"]]
 
-    [:div {:style {:flex 1}}
+    [:div {:style {:flex 1
+                   :padding-left "2em"}}
      [:h2 "HTML"]
      [:textarea
       {:on-change (fn [e]
@@ -74,7 +106,9 @@
                :height "500px"
                :width "100%"}}]
      [:button
-      {:on-click #(copy-to-clipboard (->html @text-state))
+      {:on-click (fn []
+                   (copy-to-clipboard (->html @text-state))
+                   (flash "HTML copied to clipboard!"))
        :style {:background-color :green
                :padding "1em"
                :color :white
